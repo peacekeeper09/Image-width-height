485191e2eecde6e8ed61fc0d6066ebee1920593c
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-03-11 04:30:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,21 @@ const imagePreview = document.getElementById('imagePreview');
 const imageSize = document.getElementById('imageSize');
 
 imageInput.addEventListener('change', function() {
-	const file = this.files[0];
-	const reader = new FileReader();
+  const file = this.files[0];
+  const reader = new FileReader();
 
-	reader.addEventListener('load', function() {
-		imagePreview.src = reader.result;
-		const img = new Image();
-		img.addEventListener('load', function() {
-			const width = this.width;
-			const height = this.height;
-			imageSize.textContent = `Width: ${width}px, Height: ${height}px`;
-		});
-		img.src = reader.result;
-	});
+  reader.addEventListener('load', function() {
+    imagePreview.src = reader.result;
+    const img = new Image();
+    img.addEventListener('load', function() {
+      const width = this.width;
+      const height = this.height;
+      const size = file.size;
+      const sizeInKB = Math.round(size / 1024);
+      imageSize.textContent = `Width: ${width}px, Height: ${height}px, Size: ${sizeInKB}KB`;
+    });
+    img.src = reader.result;
+  });
 
-	reader.readAsDataURL(file);
+  reader.readAsDataURL(file);
 });
